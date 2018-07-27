867ca16d9f3df2aea60603f14b6a758990769394
santi4524 <31700704+santi4524@users.noreply.github.com>
2018-07-27 10:22:57 +0700

    Update webservice.php

--- webservice.php
+++ webservice.php
@@ -1,13 +1,33 @@
-<php
+<?php 
+header('Content-Type: text/plain'); 
 
-$ws = new SoapClient('http://61.47.2.90/www/webservice/Service1.asmx?wsdl',array('trace'=>true));
-try{
-	$param = array('UserName'=>'santi','Password'=>'santi');
-	$result = $ws->GetData($param)->CheckloginResult;
-}catch(SoapFault $ex) {
-	print &ex;
+try { 
+            $options = array( 
+                'soap_version'=>SOAP_1_1, 
+                'exceptions'=>true, 
+                'trace'=>1, 
+                'cache_wsdl'=>WSDL_CACHE_NONE 
+            ); 
+            $client = new SoapClient('http://61.47.2.90/www/webservice/ServiceJson.asmx?wsdl', $options); 
+    // Note where 'Get' and 'request' tags are in the XML 
+            
+        } catch (Exception $e) { 
+            echo "<h2>Exception Error!</h2>"; 
+            echo $e->getMessage(); 
+        } 
 
-}
-echo "result :" . $result;
+//echo 'running HelloWorld :';                                      
 
-?>
+try { 
+    $param = array('UserName'=>'santi','Password'=>'santi');
+   $response=$client->GetData($param); 
+   
+   
+} 
+catch (Exception $e) 
+{ 
+    echo 'Caught exception: ',  $e->getMessage(), "\n"; 
+} 
+
+print_r($response); 
+?> 
